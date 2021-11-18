7eacec133f605adf04fbd635be286d123b274594
andreupasalamar <andreupas23@gmail.com>
2021-11-18 20:48:14 +0100

    flyway clean

--- build/resources/main/db/migration/V3__creataulausers.sql
+++ build/resources/main/db/migration/V3__creataulausers.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     username text,
-    password text -- TODO(desar contra de forma més segura)
+    password text
 );
-
+-- TODO(desar contra de forma més segura)
 INSERT INTO users VALUES ( 'user1' , 'pass' );
